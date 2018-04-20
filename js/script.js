2cbf49e0448be12bae1c51f8f4aeede49c44dd0f
Bigeard <robin.bigeard@gmail.com>
2018-04-20 16:07:46 +0200

    +++

--- js/script.js
+++ js/script.js
@@ -1,29 +1,9 @@
-onClickLogin = () => {
+onClick = () => {
     document.getElementById("div1").style.display = "none";
     document.getElementById("div2").style.display = "block";
 }
 
 onClickBack = () => {
     document.getElementById("div2").style.display = "none";
-    document.getElementById("div3").style.display = "none";
-    document.getElementById("div4").style.display = "none";
     document.getElementById("div1").style.display = "block";
-}
-
-var select = 0;
-onClickContact = () => {
-    document.getElementById("div3").style.display = "block";
-    if(select >= 1){
-        document.getElementById("send").style.display = "block";  
-    }
-    select++
-}
-
-onClickAppointment = () => {
-    document.getElementById("div4").style.display = "block";
-    if(select >= 1){
-        document.getElementById("send").style.display = "block";  
-
-    }
-    select++
 }
