9550f878c9285c14d49eacfb0a46dc8ebc4be16d
Bigeard <robin.bigeard@gmail.com>
2018-04-23 09:35:58 +0200

    colapse +++

--- js/script.js
+++ js/script.js
@@ -7,3 +7,16 @@ onClickBack = () => {
     document.getElementById("div2").style.display = "none";
     document.getElementById("div1").style.display = "block";
 }
+
+// window.addEventListener('scroll', function(evt) {
+//     document.getElementById("navbar").style.display = "block";
+// });
+
+
+// onNavbar = () => {
+//     setTimeout(() => {
+//         document.getElementById("navbar").style.display = "none";
+//         console.log("eddd")
+//     }, 300);
+
+// }
